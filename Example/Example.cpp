ffdfbbdc1155eb7db44dd2e4c084151169519cb2
Jordan <jordan.me2@gmail.com>
2017-10-13 20:12:50 +0100

    FS_ function prefix

--- Example/Example.cpp
+++ Example/Example.cpp
@@ -5,9 +5,11 @@
 template<typename T>
 void FS_CLASS( Example )<T>::DoStuff( int* data )
 {
-    int32_v a = SetAll_i32( 1 );
+    int32_v a = FS_SetAll_i32( 1 );
 
-    Store_i32( data, a );
+    FS_Store_i32( data, a );
+
+    
 }
 
 template<>
@@ -17,9 +19,9 @@ void FS_CLASS( Example )<FS_MULTI_TEMPLATE( FastSIMD_SSE2 )>::DoStuff( int* data
 
     a += _mm_set_epi32( 2, 3, 4, 5 ); 
 
-    a -= _mm_castps_si128( SetAll_f32(1.0f) );
+    a -= _mm_castps_si128( FS_SetAll_f32(1.0f) );
 
-    Store_i32( data, a );
+    FS_Store_i32( data, a );
 }
 
 FASTSIMD_BUILD_CLASS( Example )
--- FastSIMD/simdtypes/FunctionList.h
+++ FastSIMD/simdtypes/FunctionList.h
@@ -3,22 +3,22 @@
 // Set all values in the vector
 // Compile time constant when using a const value
 // I
-// float32_v Set1_f32( float value )
-#define SetAll_f32 FS::SetAll_f32
+// float32_v FS_SetAll_f32( float value )
+#define FS_SetAll_f32 FS::SetAll_f32
 
 // Set all values in the vector
 // Compile time constant when using a const value
 // I
-// int32_v Set1_i32( int32_t value )
-#define SetAll_i32 FS::SetAll_i32
+// int32_v FS_SetAll_i32( int32_t value )
+#define FS_SetAll_i32 FS::SetAll_i32
 
 // Copies all elements of the vector to given memory location
 // I
-// void Store_f32( float* ptr, float32_v toStore )
-#define Store_f32 FS::Store_f32
+// void FS_Store_f32( float* ptr, float32_v toStore )
+#define FS_Store_f32 FS::Store_f32
 
 // Copies all elements of the vector to given memory location
 // I
-// void Store_i32( void* ptr, int32_v toStore )
-#define Store_i32 FS::Store_i32
+// void FS_Store_i32( void* ptr, int32_v toStore )
+#define FS_Store_i32 FS::Store_i32
 
