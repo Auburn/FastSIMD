a4d7e3a1eee825884d14fd29bd4fdfc0ffdd5ab2
Auburn <jordan.me2@gmail.com>
2024-01-09 23:13:34 +0000

    Header only example

---
+++ examples/header_only/main.cpp
@@ -0,0 +1,14 @@
+#include <FastSIMD/ToolSet.h>
+
+int main()
+{
+    auto distSq = FS::LoadIncremented<FS::f32<4>>();
+
+    auto invSqrt = FS::InvSqrt( distSq );
+
+    auto dist = invSqrt * distSq;
+
+    auto out = FS::Masked( invSqrt != FS::f32<4>( INFINITY ), dist );
+
+    return 0;
+}
